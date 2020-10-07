3a68fd2bdbe109036e25ca15d0fa71db29ae7048
Steven Xia <stevenxia@protonmail.com>
2020-10-07 12:25:08 -0700

    Containerize bubble animation and minify. :D

--- assets/js/header-animation.js
+++ assets/js/header-animation.js
@@ -1,126 +1,128 @@
-const _BubbleBorderFactor = 0.0;
-const _BubbleSizeMax = 40;
-const _BubbleSizeMin = 25;
-const _BubbleVelocityMin = 0.1;
-const _BubbleVelocityMax = 0.3;
+(function (bubbleAnimation) {
+    const _BubbleBorderFactor = 0.0;
+    const _BubbleSizeMax = 40;
+    const _BubbleSizeMin = 25;
+    const _BubbleVelocityMin = 0.1;
+    const _BubbleVelocityMax = 0.3;
 
-const _PixelsPerBubble = 40000;
+    const _PixelsPerBubble = 40000;
 
-const _BubbleArray = [];
+    const _BubbleArray = [];
 
 // Global variables ----------------------------------------------------------
 
-const header = document.getElementById("page-header");
-const headerText = document.getElementById("page-header-text");
+    const header = document.getElementById("page-header");
+    const headerText = document.getElementById("page-header-text");
 
-let headerHeight, headerWidth, lastBubbleCreation;
+    let headerHeight, headerWidth, lastBubbleCreation;
 
 // Bubble class --------------------------------------------------------------
 
-class Bubble {
-    constructor() {
-        this.radius = (_BubbleSizeMax - _BubbleSizeMin) * Math.random() + _BubbleSizeMin;
-        this.diameter = 2 * (1 + _BubbleBorderFactor) * this.radius;
-        this.positionX = (headerWidth - this.diameter) * Math.random();
-        this.translateDistance = 0;
+    class Bubble {
+        constructor() {
+            this.radius = (_BubbleSizeMax - _BubbleSizeMin) * Math.random() + _BubbleSizeMin;
+            this.diameter = 2 * (1 + _BubbleBorderFactor) * this.radius;
+            this.positionX = (headerWidth - this.diameter) * Math.random();
+            this.translateDistance = 0;
 
-        this.velocity = ((_BubbleVelocityMax - _BubbleVelocityMin) * Math.random() + _BubbleVelocityMin);
-        this.animationStart = undefined;
+            this.velocity = ((_BubbleVelocityMax - _BubbleVelocityMin) * Math.random() + _BubbleVelocityMin);
+            this.animationStart = undefined;
 
-        this._initHTML();
-    }
+            this._initHTML();
+        }
 
-    _initHTML() {
-        this.htmlObject = document.createElement("span");
-        this.htmlObject.classList.add("bubble");
-        this.htmlObject.style.borderWidth = (_BubbleBorderFactor * this.radius) + "px";
-        this.htmlObject.style.padding = this.radius + "px";
-        this.htmlObject.style.borderRadius = (
-            parseInt(this.htmlObject.style.padding, 10)
-            + parseInt(this.htmlObject.style.borderWidth, 10)
-        ) + "px";
-        this.htmlObject.style.top = headerHeight + "px";
-        this.htmlObject.style.left = this.positionX + "px";
-        header.prepend(this.htmlObject);
-    }
+        _initHTML() {
+            this.htmlObject = document.createElement("span");
+            this.htmlObject.classList.add("bubble");
+            this.htmlObject.style.borderWidth = (_BubbleBorderFactor * this.radius) + "px";
+            this.htmlObject.style.padding = this.radius + "px";
+            this.htmlObject.style.borderRadius = (
+                parseInt(this.htmlObject.style.padding, 10)
+                + parseInt(this.htmlObject.style.borderWidth, 10)
+            ) + "px";
+            this.htmlObject.style.top = headerHeight + "px";
+            this.htmlObject.style.left = this.positionX + "px";
+            header.prepend(this.htmlObject);
+        }
 
-    destructor() {
-        this.htmlObject.remove();
-    }
+        destructor() {
+            this.htmlObject.remove();
+        }
 
-    outOfFrame() {
-        const outHorizontal = this.positionX >= headerWidth - this.diameter;
+        outOfFrame() {
+            const outHorizontal = this.positionX >= headerWidth - this.diameter;
 
-        const positionY = parseInt(this.htmlObject.style.top, 10) - this.translateDistance;
-        const outVertical = (
-            positionY <= window.scrollY - this.diameter
-            || positionY > headerHeight
-        );
+            const positionY = parseInt(this.htmlObject.style.top, 10) - this.translateDistance;
+            const outVertical = (
+                positionY <= window.scrollY - this.diameter
+                || positionY > headerHeight
+            );
 
-        return outHorizontal || outVertical;
-    }
+            return outHorizontal || outVertical;
+        }
 
-    updateRender(timestamp) {
-        if (this.animationStart === undefined)
-            this.animationStart = timestamp;
-        const elapsedTime = timestamp - this.animationStart;
+        updateRender(timestamp) {
+            if (this.animationStart === undefined)
+                this.animationStart = timestamp;
+            const elapsedTime = timestamp - this.animationStart;
 
-        this.translateDistance = elapsedTime * this.velocity;
-        this.htmlObject.style.transform = "translateY(-" + this.translateDistance + "px)";
-    }
-}
-
-// Header animation ----------------------------------------------------------
-
-function _headerAnimation(timestamp) {
-    if (window.scrollY >= headerHeight) {
-        window.requestAnimationFrame(_headerAnimation);
-        return;
-    }
-
-    for (let i = _BubbleArray.length - 1; i >= 0; i--) {
-        let bubble = _BubbleArray[i];
-
-        if (bubble.outOfFrame()) {
-            _BubbleArray.splice(i, 1);
-            bubble.destructor();
-        } else {
-            bubble.updateRender(timestamp);
+            this.translateDistance = elapsedTime * this.velocity;
+            this.htmlObject.style.transform = "translateY(-" + this.translateDistance + "px)";
         }
     }
 
-    if (lastBubbleCreation === undefined)
-        lastBubbleCreation = 0;
+// Header animation ----------------------------------------------------------
 
-    const maximumBubbles = ((headerHeight - window.scrollY) * headerWidth) / _PixelsPerBubble;
-    const bubbleLifetime = (headerHeight - window.scrollY) / ((_BubbleVelocityMax + _BubbleVelocityMin) / 2);
-    const bubbleCreationCooldown = bubbleLifetime / maximumBubbles;
+    function _headerAnimation(timestamp) {
+        if (window.scrollY >= headerHeight) {
+            window.requestAnimationFrame(_headerAnimation);
+            return;
+        }
 
-    if (_BubbleArray.length < maximumBubbles && timestamp >= lastBubbleCreation + bubbleCreationCooldown) {
-        _BubbleArray.push(new Bubble());
-        lastBubbleCreation = timestamp;
+        for (let i = _BubbleArray.length - 1; i >= 0; i--) {
+            let bubble = _BubbleArray[i];
+
+            if (bubble.outOfFrame()) {
+                _BubbleArray.splice(i, 1);
+                bubble.destructor();
+            } else {
+                bubble.updateRender(timestamp);
+            }
+        }
+
+        if (lastBubbleCreation === undefined)
+            lastBubbleCreation = 0;
+
+        const maximumBubbles = ((headerHeight - window.scrollY) * headerWidth) / _PixelsPerBubble;
+        const bubbleLifetime = (headerHeight - window.scrollY) / ((_BubbleVelocityMax + _BubbleVelocityMin) / 2);
+        const bubbleCreationCooldown = bubbleLifetime / maximumBubbles;
+
+        if (_BubbleArray.length < maximumBubbles && timestamp >= lastBubbleCreation + bubbleCreationCooldown) {
+            _BubbleArray.push(new Bubble());
+            lastBubbleCreation = timestamp;
+        }
+
+        window.requestAnimationFrame(_headerAnimation);
     }
-
-    window.requestAnimationFrame(_headerAnimation);
-}
 
 // Header resize -------------------------------------------------------------
 
-function headerResize() {
-    const minimumHeight = (
-        parseInt(window.getComputedStyle(header).paddingTop, 10)
-        + parseInt(window.getComputedStyle(header).paddingBottom, 10)
-        + parseInt(window.getComputedStyle(headerText).height, 10)
-    );
+    bubbleAnimation.onresize = function () {
+        const minimumHeight = (
+            parseInt(window.getComputedStyle(header).paddingTop, 10)
+            + parseInt(window.getComputedStyle(header).paddingBottom, 10)
+            + parseInt(window.getComputedStyle(headerText).height, 10)
+        );
 
-    headerHeight = Math.max(minimumHeight, window.innerHeight);
-    headerWidth = parseInt(window.getComputedStyle(header).width, 10)
+        headerHeight = Math.max(minimumHeight, window.innerHeight);
+        headerWidth = parseInt(window.getComputedStyle(header).width, 10)
 
-    header.style.height = headerHeight + "px";
-    headerText.style.marginTop = ((headerHeight - minimumHeight) / 2) + "px";
-}
+        header.style.height = headerHeight + "px";
+        headerText.style.marginTop = ((headerHeight - minimumHeight) / 2) + "px";
+    }
 
 // On-load functions ---------------------------------------------------------
 
-headerResize();
-window.requestAnimationFrame(_headerAnimation);
+    bubbleAnimation.onresize();
+    window.requestAnimationFrame(_headerAnimation);
+}(window.bubbleAnimation = {}));
