772167d2045270c68841b147ec141f6d1a4aecbb
Steven Xia <stevenxia@protonmail.com>
2020-10-10 03:50:17 -0700

    Remove redundant character in minify script.

--- minifiy.sh
+++ minifiy.sh
@@ -40,14 +40,14 @@ HTMLCurrOpts="\
 html-minifier $HTMLCurrOpts index.dev.html >index.html
 before=$(wc index.dev.html | awk '{print $3}')
 after=$(wc index.html | awk '{print $3}')
-change=$((100 * $after / $before - 100))
+change=$((100 * after / before - 100))
 echo "index.dev.html: $before -> $after ($change%)"
 
 # npm install csso-cli
 csso assets/css/style.css --output assets/css/style.min.css
 before=$(wc assets/css/style.css | awk '{print $3}')
 after=$(wc assets/css/style.min.css | awk '{print $3}')
-change=$((100 * $after / $before - 100))
+change=$((100 * after / before - 100))
 echo "assets/css/style.css: $before -> $after ($change%)"
 
 # npm install npx && npm install google-closure-compiler
@@ -57,7 +57,7 @@ npx google-closure-compiler \
   --compilation_level SIMPLE
 before=$(wc assets/js/echo-1.7.3.js | awk '{print $3}')
 after=$(wc assets/js/echo-1.7.3.min.js | awk '{print $3}')
-change=$((100 * $after / $before - 100))
+change=$((100 * after / before - 100))
 echo "assets/js/echo-1.7.3.js: $before -> $after ($change%)"
 
 npx google-closure-compiler \
@@ -66,7 +66,7 @@ npx google-closure-compiler \
   --compilation_level SIMPLE
 before=$(wc assets/js/echo-driver.js | awk '{print $3}')
 after=$(wc assets/js/echo-driver.min.js | awk '{print $3}')
-change=$((100 * $after / $before - 100))
+change=$((100 * after / before - 100))
 echo "assets/js/echo-driver.js: $before -> $after ($change%)"
 
 npx google-closure-compiler \
@@ -75,5 +75,5 @@ npx google-closure-compiler \
   --compilation_level SIMPLE
 before=$(wc assets/js/header-animation.js | awk '{print $3}')
 after=$(wc assets/js/header-animation.min.js | awk '{print $3}')
-change=$((100 * $after / $before - 100))
+change=$((100 * after / before - 100))
 echo "assets/js/header-animation.js: $before -> $after ($change%)"
