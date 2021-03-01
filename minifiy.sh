616bab488d75fd6ae63e53be075db3d66dd67408
Steven Xia <stevenxia@protonmail.com>
2021-02-28 22:03:31 -0800

    Fix commands for the uninitiated user (me).

--- minifiy.sh
+++ minifiy.sh
@@ -36,21 +36,21 @@ HTMLCurrOpts="\
     --sort-class-name \
     --use-short-doctype"
 
-# npm install html-minifier
+# npm install html-minifier --global
 html-minifier $HTMLCurrOpts index.dev.html >index.html
 before=$(wc index.dev.html | awk '{print $3}')
 after=$(wc index.html | awk '{print $3}')
 change=$((100 * after / before - 100))
 echo "index.dev.html: $before -> $after ($change%)"
 
-# npm install csso-cli
+# npm install csso-cli --global
 csso assets/css/style.css --output assets/css/style.min.css
 before=$(wc assets/css/style.css | awk '{print $3}')
 after=$(wc assets/css/style.min.css | awk '{print $3}')
 change=$((100 * after / before - 100))
 echo "assets/css/style.css: $before -> $after ($change%)"
 
-# npm install npx && npm install google-closure-compiler
+# npm install npx --global && npm install google-closure-compiler --global
 npx google-closure-compiler \
   --js=assets/js/echo-1.7.3.js \
   --js_output_file=assets/js/echo-1.7.3.min.js \
